4512cd7f37d51b8a8705ad8a1047c0262a3e2289
NeThounet <nathan.mari@numericable.fr>
2020-08-04 20:53:29 +0200

    Updates

---
+++ precache-manifest.1f0bd111061ae699a792e4faf5e8abc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c54a7d873bb0275909b2",
+    "url": "/quizzarbitre/static/css/main.e68c5e9c.chunk.css"
+  },
+  {
+    "revision": "c54a7d873bb0275909b2",
+    "url": "/quizzarbitre/static/js/main.c54a7d87.chunk.js"
+  },
+  {
+    "revision": "51bd8e18581b158ad637",
+    "url": "/quizzarbitre/static/js/1.51bd8e18.chunk.js"
+  },
+  {
+    "revision": "2f4b8d238373995a6031",
+    "url": "/quizzarbitre/static/js/runtime~main.2f4b8d23.js"
+  },
+  {
+    "revision": "b4084e44bea589ec3800fdfdd69cf189",
+    "url": "/quizzarbitre/static/media/icon-check.b4084e44.svg"
+  },
+  {
+    "revision": "05a3e30fdf60ebe9d00cf4725d44bd42",
+    "url": "/quizzarbitre/static/media/logo.05a3e30f.svg"
+  },
+  {
+    "revision": "2986945b44f0828276dcc7c8cc20c279",
+    "url": "/quizzarbitre/index.html"
+  }
+];
